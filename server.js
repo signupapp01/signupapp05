e8906758bd4d010d4027d7f56c7c2d7f22c7aca3
dev <dev@muvus.co.uk>
2016-04-09 15:20:15 +0100

    2nd commit

---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require('express'),
+  mongoose = require('mongoose');
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('error-handler'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+// DB configuration
+var configDB = require('./config/database.js'); // require config.js file
+mongoose.connect(configDB.url); // connect to our database
+
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var env = process.env.NODE_ENV || 'development';
+
+// development only
+/*if (env === 'development') {
+  app.use(express.errorHandler());
+}*/
+
+// production only
+if (env === 'production') {
+  // TODO
+}
+
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+app.post('/api/save-register-form', api.saveForm);
+app.post('/api/get-user-data', api.getCustomer);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Start Server
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
